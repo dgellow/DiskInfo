308e718ca2c143312a055aa3c9aba799cd773db9
dgellow <samuel.elborai@gmail.com>
2015-10-26 06:51:38 +0100

    Initial Commit

---
+++ DiskInfo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DiskInfo
+//
+//  Created by Samuel El-Borai on 26/10/15.
+//  Copyright © 2015 Samuel El-Borai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
