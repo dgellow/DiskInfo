70babc4f0a3122a32396dcbf59bebc1631d19aac
dgellow <samuel.elborai@gmail.com>
2015-10-26 15:10:05 +0100

    Initial commit

--- DiskInfo/AppDelegate.swift
+++ DiskInfo/AppDelegate.swift
@@ -10,18 +10,65 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    
+    override func awakeFromNib() {
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.menu = menu
+        statusBarItem.image = NSImage(imageLiteral: "icon16_white.png")
+        statusBarItem.button?.action = Selector("populateMenu")
+        
+        populateMenu()
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    
+    func populateMenu() {
+        menu.removeAllItems()
+        getDiskInfo().forEach({
+            (info: String) in
+            let menuItem : NSMenuItem = NSMenuItem()
+            menuItem.title = info
+            menuItem.keyEquivalent = ""
+            menu.addItem(menuItem)
+        })
+        
+        let menuItemQuit = NSMenuItem()
+        menuItemQuit.title = "Quit DiskInfo"
+        menuItemQuit.action = Selector("terminateApp")
+        menuItemQuit.keyEquivalent = ""
+        menu.addItem(menuItemQuit)
     }
-
-
+    
+    func terminateApp() {
+        exit(0)
+    }
+    
+    func getDiskInfo() -> [String] {
+        let attributes = directoryFileSystemAttributes("/")!
+        let filesystemSize = attributes[NSFileSystemSize] as! Int
+        let filesystemFreeSize = attributes[NSFileSystemFreeSize] as! Int
+        let totalSize = BytesToGigaBytes(filesystemSize)
+        let freeSize = BytesToGigaBytes(filesystemFreeSize)
+        
+        return [
+            "Total: \(totalSize) GiB",
+            "Free: \(freeSize) GiB",
+            "Used: \(totalSize - freeSize) GiB",
+            "Percentage used: \(Int((Double(totalSize) - Double(freeSize)) / (Double(totalSize) / 100))) %",
+        ]
+    }
+    
+    func directoryFileSystemAttributes(path: String) -> [String: AnyObject]? {
+        do {
+            return try NSFileManager.defaultManager().attributesOfFileSystemForPath(path)
+        } catch {
+            return nil
+        }
+    }
+    
+    func BytesToGigaBytes(b: Int) -> Int {
+        return b / 1024 / 1024 / 1024
+    }
 }
 
